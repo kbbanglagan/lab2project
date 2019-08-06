4baf7dda70a3bc6606338f14a01110abfdb80901
Raxell Gomez <raxellgomez@gmail.com>
2019-08-06 08:13:59 +0800

    no message

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+img {
+  border-radius: 8px;
+}
+</style>
+	<title>Lab2</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="background">
+
+<div class="nav">
+	<a class="a active" href="#">HOME</a>
+	<a class="b" href="#">ABOUT ME</a>
+	<a class="c" href="#">ACTIVITIES</a>
+	<a class="d" href="#">REFLECTIONS</a>
+	<a class="e" href="#">PROJECTS</a>
+
+</div>
+
+<div class="content">
+	
+	<div class="home">
+
+		<img src="images/dp.jpg" alt="dp" width="300" height="300" class="center">
+		<h1> I am Kevin! </h1>
+		<h3> I.T student in Asia Pacific College </h3>
+	</div>
+
+	<div class="aboutme">
+		<img src="images/dp2.jpg" alt="dp" width="300" height="300" class="center">
+		<h1> BSIT-MI181 </h1>
+		<p><b>I'm Kevin Josef Banglagan.</b><br> 
+		You can call me Kevin. I'm 18 years old from Pasay City. I graduated from St. Mary's Academy - Pasay City (GS/JHS/SHS). Being in one school for 12 years and received a loyalty award. It's very hard for my parents to have me go to a decent school to study. I’m the youngest child among my siblings and we are 3. My sister is a nurse working on dubai and my brother is a chemical engineer and me still studying to pursue my chosen course and graduate as an I.T student. My hobbies are listening to music, play basketball and play computer games.</p>
+
+		<p> I'm more as a computer tech guy rather than playing console. I spend a lot of time on computer or playing computer games with friends. I like building, cleaning computers and help friends who are in need creating their own computer setup. My first game that I got addicted to was League of Legends (PC game). When I was at highschool, my classmates are always going to a computer shop after dismissal and play computer games. I got jealous to them because their having fun while I'm just browsing the internet back in the days to watch Youtube. They convince me to play the game with them, after a while trying the game, I got addicted easily and wanted to play the game everyday. I wanted to travel to Korea, Japan, US to experience what's like being there because I've been to Canada when I was young. I followed tech guys on social media platforms like (Linus Tech Tips, Bitwit, JayzTwoCents, GamerNexus, etc..). I wanted to be like them to help and also people trying to learn new things about technologies. I tend to eat a lot of food when I'm alone while using computer. Most of the time I watch tv series, vlogs, gameplays so I can sleep because I don’t know why. I got the inspiration on computers when I was grade 5. My first computer was when I'm 11 years old with my brother-in-law. He helps me creating my first computer setup and he was an I.T too, that also makes me get inspired and chose I.T as my course. </p>
+
+	</div>
+
+	<div class="activities">
+		<h1> Activity #2 </h1>
+		<p> My favorite app/s that I used is Discord. Why Discord? It’s like skype but you only use headsets with microphone, earphones or in loudspeaker on phone to use voice chat or you can just use chat to communicate with your friends or discord user. I like it because it’s All-in-One voice and text chat for gamers. Features of this app is it’s free, you can download it on desktop or mobile. Secure, because it needs your email to login to the app. You can add music bot on the channel to listen music while playing games with your gamers. </p>
+		<h1> Activity #3 </h1>
+		<p> I would design an interface on elevators that have a television that shows every floor of the building. It has mini A.C. There will be 3 elevators. You will put the floor number, then you’re going there at a speed of 35 mph to your elevator’s location. It takes 5 minutes from basement to 350th floor. Another 5 minutes from 351th to 750th and another 5 minutes for 751th to 1000th. It has a capacity of 35 people with priority seating for people with disabilities, elderly, pregnant women. It has music so the passengers will not be bored. The elevator doors are double so you can exit both ways. I got the inspiration from the hospital, Makati Medical Center wherein there elevators have both doors so that people can get off with ease. </p>
+		<h1> Activity #4 </h1>
+		<p> Problem:</p>
+		<p>In the current status quo of our society, people have the capability to think of travelling on places they haven`t visited yet. They are enticed with the beautiful views of nature along with its different activities and events to begin with. People are always looking forward on creating unforgettable moments, experiences and memories with or without their love ones.
+		Searching different places for possible vacation would be easy due to today`s technology but, the problem begins when there are varieties of information on the internet and people are confused because when these are compared on each other it produces unmatched result.</p>
+
+		<p>Needs:</p>
+		<p>•	A travel guide that contains information about different tourist’s spot, restaurant, do’s and don’ts, etiquettes, and basic language of a certain country.</p>
+		<p>•	Gathered precise information as a guidance on travelling to different places. </p>
+
+	</div>
+
+	<div class="reflections">
+		<h1> Reflection #1 </h1>
+		<p> My experience on the first week of USERDES class was great. This is my first time taking the subject so I don’t have yet the knowledge or information what we are about to tackle in. I like the way Miss Jen talks about what is USERDES and gives us information what it is about. She makes us comfortable in many ways just to learn and make the best out of us. She let us bring our best and think critically in doing presentations to have a good work or make presentable to others. The tool we used in collaborating with my blockmate’s is Trello. Trello is very new to me, I don’t have yet experienced using it. Most of us don’t know to how to use it. We explore first about the website. Some of us are having fun while exploring trello like if one is changing the background all of us will have the same background, you can see the other cards or profiles, you can add stickers to your own card or to other people, you can see the history of activities. Miss Jen told us that it’s okay to explore first to learn new things about it. She makes do activity that tell about ourselves in a creative way. She tell us about our favorite app and show it to the class. Now she wants us to create our own user interface for a 1000th elevators. Our creative minds now show what we can do. Designing an elevator is very hard because you need to consider what are the surroundings, limitations, etc. We share the same ideas on what the elevator look like inside the class and collect the same ideas and have a conclusion about it. Designing something takes time because you have to put an effort and make it work before putting it on a test. </p>
+		<h1> Reflection #2 </h1>
+		<p> Second week in USERDES, we tackled about designing and creating an app that can help solve real life problems in our day to day life. Need finding is a process that we need to provide a solution on a problem. Having solutions on a problem is hard. Finding first what are the possible solutions for a specific problem. On our group, we think of an idea wherein we will create a feedback app for professors every after class so they can monitor what can be change in the way of their teaching and giving activities for the class.</p> 
+
+		<p>Teamwork is needed to accomplish a goal. It doesn’t mean everybody doing the same thing or everybody being able to do each other’s jobs. It’s more a means to a synergistic way of working, where the sum is greater than the parts. Properly managed, teamwork maximizes strengths, bringing out the best in each team member, a key theme on this site.</p>
+		<h1> Reflection #3 </h1>
+		<p> On the 3rd week of User Design class, we’re given a task to create a personal website using HTML and CSS. I have a little background about this when I was high school. I don’t remember any how to do a website. That’s why I have hard time doing the task so watch some on youtube how to do it.</p>
+
+		<p>Github is one of the most used git version control repository servers. We’ve been using Github on our subject Mobile Programming. It’s good for hosting codes. When I was new at this, the function and purpose of the commit changes are a bit of confusing because I can’t find the buttons, but with the help of my classmates, I’ve can use it properly. Also, it makes a little harder to use because of no search button on repositories. Heroku is very confusing at first, but I ask for assistance from classmate how to connect to github. It was really easy when they taught me how to do it.</p>
+		<h1> Reflection #4</h1>
+		<p> Userinyerface is a unique quiz we’ve done on our course because you need to use your common sense, patience and logic thinking. On my first try, it took me 19 minutes before figuring out because it’s not a typical user interface you see on the internet. I got stuck on filling up personal information because I don’t know what I will input. I got annoyed and mostly give-up but we need to finish it on time. After my first try, I try it again and I finished it in 2mins up.</p>
+		
+		<p>You just need to be calm to think carefully and analyze it. Designing a user interface is hard because it needs to be user-friendly as possible. Messy/Bad designing can lead to confusions and frustration like this one. It can make an impact on you and the user because all your effort can be nothing.</p>
+
+	</div>
+	<div class="projects" style="display: none;">
+		<h1> For my Lab 1 Project, </h1>
+		<a href="https://userdeslab1project.herokuapp.com" target="_blank"> Click here! </a>
+	</div>
+
+</div>
+
+
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		$('a').click(function(){
+			//alert("i am click");
+			var selected = $(this);
+			$('a').removeClass('active');
+			$(selected).addClass('active');
+		});
+
+		var $a = $('.a'),
+		$b = $('.b'),
+		$c = $('.c'),
+		$d = $('.d'),
+		$e = $('.e'),
+		$home = $('.home'),
+		$aboutme = $('.aboutme'),
+		$activities = $('.activities'),
+		$reflections = $('.reflections'),
+		$projects = $('.projects');
+
+		$a.click(function(){
+			$home.fadeIn();
+			$aboutme.fadeOut();
+			$activities.fadeOut();
+			$reflections.fadeOut();
+			$projects.fadeOut();
+		});
+		$b.click(function(){
+			$home.fadeOut();
+			$aboutme.fadeIn();
+			$activities.fadeOut();
+			$reflections.fadeOut();
+			$projects.fadeOut();
+		});
+		$c.click(function(){
+			$home.fadeOut();
+			$aboutme.fadeOut();
+			$activities.fadeIn();
+			$reflections.fadeOut();
+			$projects.fadeOut();
+		});
+		$d.click(function(){
+			$home.fadeOut();
+			$aboutme.fadeOut();
+			$activities.fadeOut();
+			$reflections.fadeIn();
+			$projects.fadeOut();
+		});
+		$e.click(function(){
+			$home.fadeOut();
+			$aboutme.fadeOut();
+			$activities.fadeOut();
+			$reflections.fadeOut();
+			$projects.fadeIn();
+		});
+	});
+
+</script>
+
+
+</body>
+</html>
